6aa6bc398a0f764e2b33d83b6ff1247672742acd
Bartosz Sądel <bartosz.sadel@onwelo.com>
2018-10-06 08:09:14 +0200

    removed unused imports

--- src/test/java/com/onwelo/presentation/junit5/Junit4ParametrizedTest.java
+++ src/test/java/com/onwelo/presentation/junit5/Junit4ParametrizedTest.java
@@ -1,24 +1,15 @@
 package com.onwelo.presentation.junit5;
 
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.MockitoRule;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public class Junit4ParametrizedTest {
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {{0,0}, {1,1}, {2,2}});
-    }
 
     private int input;
 
@@ -27,6 +18,11 @@ public class Junit4ParametrizedTest {
     public Junit4ParametrizedTest(int input, int output) {
         this.input = input;
         this.output = output;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{{0, 0}, {1, 1}, {2, 2}});
     }
 
     @Test
